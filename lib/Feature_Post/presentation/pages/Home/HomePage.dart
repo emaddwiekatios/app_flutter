d19fd547b546efae7fdea0cd23118901c7a27d14
emaddwiekatios <emad.dweikat.ios@gmail.com>
2022-12-13 15:12:03 +0200

    for commit

--- lib/Feature_Post/presentation/pages/Home/HomePage.dart
+++ lib/Feature_Post/presentation/pages/Home/HomePage.dart
@@ -5,7 +5,6 @@ import 'package:clean_arch_app/core/resource/MediaQuery.dart';
 import 'package:clean_arch_app/core/resource/ValueManger.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-//import 'dart:ui';
 import 'package:carousel_slider/carousel_slider.dart';
 
 import '../../../../core/resource/StringManager.dart';
@@ -18,28 +17,20 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-// const CURVE_HEIGHT = 160.0;
-// const AVATAR_RADIUS = CURVE_HEIGHT * 0.28;
-// const AVATAR_DIAMETER = AVATAR_RADIUS * 2;
 Color colorOne = Colors.amber;
 Color colorTwo = ColorManager.primary;
 Color colorThree = ColorManager.primary;
 
 class _HomePageState extends State<HomePage> {
-  final GlobalKey<ScaffoldState> scaffoldKey =  GlobalKey<ScaffoldState>();
-
-
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       drawer: GetDrower(context),
       key: scaffoldKey,
-
       body: GestureDetector(
         onTap: () {
-
           FocusScopeNode currentFocus = FocusScope.of(context);
 
           if (!currentFocus.hasPrimaryFocus) {
@@ -127,12 +118,15 @@ class _HomePageState extends State<HomePage> {
               top: AppSize.s4,
               left: -AppSize.s4,
               child: IconButton(
-                icon:  Icon(Icons.menu,color: ColorManager.grey2,),
+                icon: Icon(
+                  Icons.menu,
+                  color: ColorManager.grey2,
+                ),
                 onPressed: () {
                   //print('inside button');
-                   scaffoldKey.currentState!.openDrawer();
+                  scaffoldKey.currentState!.openDrawer();
 
-                //  Navigator.pushReplacementNamed(context, "/ProductsMain");
+                  //  Navigator.pushReplacementNamed(context, "/ProductsMain");
                 },
               ),
             ),
@@ -142,7 +136,10 @@ class _HomePageState extends State<HomePage> {
               child: Row(
                 children: [
                   IconButton(
-                    icon:  Icon(Icons.notification_add_sharp,color: ColorManager.grey2,),
+                    icon: Icon(
+                      Icons.notification_add_sharp,
+                      color: ColorManager.grey2,
+                    ),
                     onPressed: () {
                       Navigator.pop(context);
                       //  Navigator.popAndPushNamed(context, "/SignIn");
@@ -151,7 +148,10 @@ class _HomePageState extends State<HomePage> {
                     },
                   ),
                   IconButton(
-                    icon:  Icon(Icons.shopping_cart_outlined,color: ColorManager.grey2,),
+                    icon: Icon(
+                      Icons.shopping_cart_outlined,
+                      color: ColorManager.grey2,
+                    ),
                     onPressed: () {
                       //print('inside button');
                       // FirebaseAuth.instance.signOut();
@@ -170,12 +170,12 @@ class _HomePageState extends State<HomePage> {
               right: 0,
               bottom: 20,
               child: SizedBox(
-                  //color: Colors.red,
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height >= 775.0
-                      ? MediaQuery.of(context).size.height
-                      : 775.0,
-                  /* decoration: new BoxDecoration(
+                //color: Colors.red,
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height >= 775.0
+                    ? MediaQuery.of(context).size.height
+                    : 775.0,
+                /* decoration: new BoxDecoration(
                         gradient: new LinearGradient(
                             colors: [
                               Colors.red,
@@ -190,18 +190,11 @@ class _HomePageState extends State<HomePage> {
                             tileMode: TileMode.clamp),
                       ),
 */
-                  child:
-
-                  (MediaQuery.of(context).orientation ==Orientation.portrait) ?
-                 const GetPortraitOrient()
-                :const GetLandScapeOrient(),
-
-
-
-              )
-
-
-              ,
+                child:
+                    (MediaQuery.of(context).orientation == Orientation.portrait)
+                        ? const GetPortraitOrient()
+                        : const GetLandScapeOrient(),
+              ),
             ),
             //header title
             // Positioned(
@@ -249,19 +242,18 @@ class WidgetCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children:  [
+      children: [
         Card(
-
           shape: RoundedRectangleBorder(
-              side:BorderSide(color: ColorManager.primary,width: AppSize.s0_5)
-              ,borderRadius:BorderRadius.circular(270)),
-
+              side:
+                  BorderSide(color: ColorManager.primary, width: AppSize.s0_5),
+              borderRadius: BorderRadius.circular(270)),
           child: const CircleAvatar(
             radius: AppSize.s40,
             backgroundImage: ExactAssetImage(AssetManager.splash3),
           ),
         ),
-         const Text('Mad')
+        const Text('Mad')
         //  backgroundImage:  (AssetManager.onBoarding3,fit: BoxFit.cover))
       ],
     );
@@ -271,83 +263,76 @@ class WidgetCategory extends StatelessWidget {
 class GetCarouselSlider extends StatelessWidget {
   //class ImageSliderDemo extends StatelessWidget {
   final List<String> imgList = [
-
     AssetManager.splash1,
     AssetManager.splash2,
     AssetManager.splash3,
     AssetManager.splashLogo
   ];
 
-   GetCarouselSlider({Key? key}) : super(key: key);
+  GetCarouselSlider({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),color: Colors.transparent,
-
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.transparent,
         ),
-          //color: Colors.red,
-          height: get_height(context)* .5,
-          width: get_width(context) *.95,
-          child: CarouselSlider(
-            items: imgList
-                .map((p) =>
-                Card(
-
+        //color: Colors.red,
+        height: get_height(context) * .5,
+        width: get_width(context) * .95,
+        child: CarouselSlider(
+          items: imgList
+              .map(
+                (p) => Card(
                   shape: OutlineInputBorder(
-                    borderSide: BorderSide(color: ColorManager.primary,
-                        width: AppSize.s0_5),
+                    borderSide: BorderSide(
+                        color: ColorManager.primary, width: AppSize.s0_5),
                   ),
                   elevation: AppSize.s0,
                   child: Container(
                     decoration: BoxDecoration(
-                      image: DecorationImage(fit: BoxFit.fill,
-                          image: ExactAssetImage( p,
-                           // width: MediaQuery.of(context).
-                            // size.width,
-                            //fit: BoxFit.cover,
-                          )
-
-                      ),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-
+                        image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: ExactAssetImage(
+                              p,
+                              // width: MediaQuery.of(context).
+                              // size.width,
+                              //fit: BoxFit.cover,
+                            )),
+                        borderRadius: BorderRadius.circular(10)),
                   ),
                 ),
-            )
-                .toList(),
-            options: CarouselOptions(
-              autoPlay: true,
-              autoPlayAnimationDuration: const Duration(seconds: 2),
-
-              viewportFraction: 1.0,
-              enlargeCenterPage: false,
-              initialPage: 0,
-              onPageChanged: (index, reason) {
-                // setState(() {
-                //   currentIndex = index;
-                //   _product = _products[index];
-                // });
-              },
-            ),
+              )
+              .toList(),
+          options: CarouselOptions(
+            autoPlay: true,
+            autoPlayAnimationDuration: const Duration(seconds: 2),
+            viewportFraction: 1.0,
+            enlargeCenterPage: false,
+            initialPage: 0,
+            onPageChanged: (index, reason) {
+              // setState(() {
+              //   currentIndex = index;
+              //   _product = _products[index];
+              // });
+            },
           ),
+        ),
       ),
     );
   }
 }
-
 
 class GetPortraitOrient extends StatelessWidget {
   const GetPortraitOrient({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.only(
-              left: AppSize.s6, right: AppSize.s6),
+          padding: const EdgeInsets.only(left: AppSize.s6, right: AppSize.s6),
           child: TextFormField(
             showCursor: true,
             // readOnly: true,
@@ -367,7 +352,7 @@ class GetPortraitOrient extends StatelessWidget {
         ),
         Padding(
           padding: const EdgeInsets.all(AppSize.s4),
-          child:  SizedBox(
+          child: SizedBox(
             //  color: Colors.red,
             height: get_height(context) / FontManagerSize.s7,
             width: get_width(context),
@@ -384,7 +369,7 @@ class GetPortraitOrient extends StatelessWidget {
           child: Container(
             margin: const EdgeInsets.all(AppSize.s0_5),
             decoration: BoxDecoration(
-              //color: Colors.red,
+                //color: Colors.red,
                 borderRadius: BorderRadius.circular(10)),
 
             height: get_height(context) / AppSize.s6,
@@ -395,39 +380,37 @@ class GetPortraitOrient extends StatelessWidget {
         ),
         Expanded(
           child: Padding(
-            padding:  const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(8.0),
             child: Container(
-              decoration: const BoxDecoration(
-               
-              ),
-              width: get_width(context),//FontManagerSize.s3,
-              //color:Colors.blue,
-              child:GridView.builder(
-                  itemBuilder :(context ,int index )
-              {
-                return Card(elevation: 10,
-                  shape: OutlineInputBorder(
-                      borderSide: BorderSide(color: ColorManager.primary,
-                          width: AppSize.s0_5),
-                      borderRadius: const  BorderRadius.all(Radius.circular(AppSize.s8))),
+                decoration: const BoxDecoration(),
+                width: get_width(context), //FontManagerSize.s3,
+                //color:Colors.blue,
+                child: GridView.builder(
+                  itemBuilder: (context, int index) {
+                    return Card(
+                      elevation: 10,
+                      shape: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: ColorManager.primary, width: AppSize.s0_5),
+                          borderRadius: const BorderRadius.all(
+                              Radius.circular(AppSize.s8))),
 
-                  //borderOnForeground: true,
-                  child: Column(
-                    children: [
-
-                           Container(
-                             height: 80,width: 80,
-
-                             decoration: BoxDecoration(
-
-                               borderRadius: BorderRadius.circular(5),
-                                 image: const DecorationImage(fit:BoxFit.cover ,
-                                   image:  AssetImage(AssetManager.splash2,)
-                                 ),
-                               color:Colors.limeAccent,
-                             ),
-
-                           ),
+                      //borderOnForeground: true,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              image: const DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage(
+                                    AssetManager.splash2,
+                                  )),
+                              color: Colors.limeAccent,
+                            ),
+                          ),
 //                          CircleAvatar(
 //
 //                            backgroundImage: ExactAssetImage(AssetManager.splash2)
@@ -435,18 +418,17 @@ class GetPortraitOrient extends StatelessWidget {
 // ,radius: 50,
 //                          ),
 
-                     const  Text('rrr'),
-                     const  Text('hello')
-                    ],
-                  ),
-                );
-              },
-                itemCount: 10,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          const Text('rrr'),
+                          const Text('hello')
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: 10,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 3,
-
-                ),)
-            ),
+                  ),
+                )),
           ),
         )
       ],
@@ -454,17 +436,15 @@ class GetPortraitOrient extends StatelessWidget {
   }
 }
 
-
-
 class GetLandScapeOrient extends StatelessWidget {
   const GetLandScapeOrient({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.only(top: FontManagerSize.s12,
-              left: AppSize.s6, right: AppSize.s6),
+          padding: const EdgeInsets.only(
+              top: FontManagerSize.s12, left: AppSize.s6, right: AppSize.s6),
           child: TextFormField(
             showCursor: true,
             // readOnly: true,
@@ -486,7 +466,7 @@ class GetLandScapeOrient extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.all(AppSize.s4),
-              child:  SizedBox(
+              child: SizedBox(
                 //  color: Colors.red,
                 height: get_height(context) * .6,
                 width: get_width(context) * .25,
@@ -505,19 +485,19 @@ class GetLandScapeOrient extends StatelessWidget {
                   child: Container(
                     margin: const EdgeInsets.all(AppSize.s0_5),
                     decoration: BoxDecoration(
-                      //color: Colors.red,
+                        //color: Colors.red,
                         borderRadius: BorderRadius.circular(10)),
 
-                    height: get_height(context) *.25,
-                    width: get_width(context) *.7,
+                    height: get_height(context) * .25,
+                    width: get_width(context) * .7,
                     child: GetCarouselSlider(),
                     //child:Cal
                   ),
                 ),
                 Container(
-                  height: get_height(context)*.35,
-                  width: get_width(context)* .7,//FontManagerSize.s3,
-                  color:Colors.blue,
+                  height: get_height(context) * .35,
+                  width: get_width(context) * .7, //FontManagerSize.s3,
+                  color: Colors.blue,
                 ),
               ],
             ),
@@ -527,4 +507,3 @@ class GetLandScapeOrient extends StatelessWidget {
     );
   }
 }
-

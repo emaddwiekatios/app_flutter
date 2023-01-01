5af8220bbfb1907fc6e7bc9068362fadf5c31790
emaddwiekatios <emad.dwiekat.ios@gmail.com>
2023-01-02 01:09:10 +0200

    first update  commecnt macbranch

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
- /// android studio pc new   main
+ /// android studio pc new   main  macbranch
   @override
   Widget build(BuildContext context) {
     Locale lang = prefs!.getString('Lang') == 'ar'

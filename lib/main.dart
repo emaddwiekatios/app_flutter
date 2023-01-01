cc29fd118bb8ebbf09c9836d0ea8866449346256
emaddwiekatios <emad.dwiekat.ios@gmail.com>
2023-01-02 01:04:04 +0200

    first update  commecnt rf

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
- /// android studio pc new   eee
+ /// android studio pc new   eee    uuuuu
   @override
   Widget build(BuildContext context) {
     Locale lang = prefs!.getString('Lang') == 'ar'

b7c8c08acb771cb881cbdc2c23737c09536f92cf
imad.dweikat <emad.dwiekat.ios@gmail.com>
2023-02-17 03:03:31 +0200

    carts Pages

--- lib/Feature_Post/presentation/pages/Carts/Carts.dart
+++ lib/Feature_Post/presentation/pages/Carts/Carts.dart
@@ -337,7 +337,7 @@ class _CartsState extends State<Carts> {
                         return   CartsCeil(instCarts: instCartsList[index],);
                       },
                     ),
-                  )
+                  )//hgjghjghj
 /*
 FutureBuilder<List<UserModel>>(
 future: db.getUserModelData(),
